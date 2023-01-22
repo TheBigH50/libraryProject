68004d7b389c07080262f538112667bb60bb906c
TheBigH50 <HFangmeier@outlook.com>
2023-01-22 09:46:59 -0600

    Start working app.js
    - Create Variables for all elements
    - Change divs to sections, add article container
    - Rename bookTableDiv to bookTableSection

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+let inputForm = document.getElementById("inputForm");
+let author = document.getElementById("author");
+let pageCount = document.getElementById("pageCount");
+let checkBox = document.getElementById("checkbox");
+let startDate = document.getElementById("startDate");
+let endDate = document.getElementById("endDate");
+let bookTableSection = document.getElementById("bookTableSection");
+let bookTable = document.getElementById("bookTable");
+let btCaption = document.getElementById("btCaption");
+
