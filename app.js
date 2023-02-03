5418b334025b1e76b43d16c81758694defb24e56
TheBigH50 <HFangmeier@outlook.com>
2023-02-03 08:37:17 -0600

    Cover Art Fetch
    - Function fetchAndSetBookCover
    	- Nested fetch
    		- Gets book cover id then cover art url
    	- Sets img url and stores in library
    	- Update renderPersonalLibrary to render thumbnails
    	- Update updateModal to render "full size" image of cover art
    	- Set default svg of book as placeholder
    	- Storage of svg still broken (fetch sending url with undefined)

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let bookTable = document.getElementById("bookTable");
 let btCaption = document.getElementById("btCaption");
 let tableRows = document.getElementsByTagName("tr");
 let library = [];
+let bookSVG = 
 
 addBook.addEventListener("click", bindAndStore);
 document.addEventListener("readystatechange", getLibrary);
@@ -26,8 +27,10 @@ class Book {
     this.read = read.checked;
     this.start = start.value;
     this.end = end.value;
+    this.imgURL = "./assets/book.svg";
+    this.imgH = "200";
+    this.imgW = "200";
   }
-  beenRead() {}
 }
 
 function createBook(title, author, pages, read, start, end) {
@@ -40,6 +43,8 @@ function renderPersonalLibrary(book) {
   let tempD2 = document.createElement("td");
   let tempD3 = document.createElement("td");
   let tempD4 = document.createElement("td");
+  let tempD5 = document.createElement("td");
+  let thumbnail = document.createElement("img");
   let tempStart = Date.parse(book.start);
   let tempEnd = Date.parse(book.end);
   console.log("tempEnd:", tempEnd);
@@ -49,7 +54,18 @@ function renderPersonalLibrary(book) {
   let formattedStart = formatDate(tempStart, currentDate);
   console.log(currentDate);
   console.log(book.start, tempStart);
+  
+    thumbnail.src = `${book.imgURL}`;
+    thumbnail.alt = `${book.title} cover art`;
+    thumbnail.height = "25";
+    thumbnail.width = "25";
+  
+  tempR.setAttribute("imgURL", `${book.imgURL}`);
+  tempR.setAttribute("imgALT", `${book.title} cover art`);
+  tempR.setAttribute("imgH", `${book.imgH}`);
+  tempR.setAttribute("imgW", `${book.imgW}`);
 
+  console.log("pass the img?", tempR.getAttribute("imgURL"));
   tempR.id = `${book.title}`;
   console.log(tempR.id);
 
@@ -67,15 +83,29 @@ function renderPersonalLibrary(book) {
   }
 
   bookTable.appendChild(tempR);
+  tempR.appendChild(tempD5);
   tempR.appendChild(tempD1);
   tempR.appendChild(tempD2);
   tempR.appendChild(tempD3);
   tempR.appendChild(tempD4);
+  tempD5.appendChild(thumbnail);
+
+  /* if (book.imgH == 25 && book.imgW == 25) {
+    thumbnail.src = `${book.imgURL}`;
+    thumbnail.alt = `${book.title} cover art`;
+    thumbnail.height = book.imgH;
+    thumbnail.width = book.imgW;
+    tempD5.appendChild(thumbnail);
+    } else {
+      thumbnail = book.imgURL;
+  tempD5.appendChild(thumbnail);
+    }; */
+  
 }
 
 // This code is responsible for binding an event to the submit button and storing the data in the library. The bindAndStore() function takes the input data from the form and creates a new book object, which is then pushed to the library array. The renderPersonalLibrary() function is then called to render the new book object. Finally, the storeLibrary() function is called to store the library array in the localStorage.
 
-function bindAndStore(event) {
+async function bindAndStore(event) {
   event.preventDefault();
   let newBook = createBook(
     titleInput,
@@ -85,11 +115,11 @@ function bindAndStore(event) {
     startDate,
     endDate
   );
-
+  fetchAndSetBookCover(newBook.title, newBook.imgURL);
   library.push(newBook);
   console.log(library);
   renderPersonalLibrary(newBook);
-  storeLibrary();
+  //storeLibrary();
 }
 
 // This code is responsible for formatting a date string, calculating the difference between two dates, and calculating an adjustment for a date. The formatDate() function takes a date string and an adjustment, calculates the adjusted date, and formats it for display. The calcDays() function calculates the difference between two dates. The calcAdjustment() function calculates the adjustment for a date to account for timezone difference vs UTC.
@@ -124,14 +154,15 @@ function calcAdjustment(utc1, utc2) {
   return utc1 + left;
 }
 
-// This code is responsible for storing data in the browser's localStorage, and for retrieving the data from localStorage. The storeLibrary() function takes data from the 'library' variable and converts it to a JSON object which is then stored in localStorage using setItem(). The getLibrary() function retrieves the JSON object from localStorage using getItem() and parses the data. The parsed data is then added back to the 'library' variable and is rendered using the renderPersonalLibrary() function.
-
+/* The storeLibrary function first removes the existing "library" item from local storage (if it exists),
+then stringifies the library object and stores it as the new "library" item in local storage. */
 function storeLibrary() {
   window.localStorage.removeItem("library");
   let jsonObj = JSON.stringify(library);
   localStorage.setItem("library", jsonObj);
 }
 
+/* The getLibrary function retrieves the "library" item from local storage, parses it back into an object, and adds each element to the library array. The function then calls the renderPersonalLibrary function for each element in the parsed object. */
 function getLibrary() {
   let jsonObj = window.localStorage.getItem("library");
   let parsed = JSON.parse(jsonObj);
@@ -141,6 +172,7 @@ function getLibrary() {
   });
 }
 
+/* The updateModal function creates and adds an update modal element to the modalPlacement element in the DOM. The modal contains a form with a date input and buttons to "Finished Reading" and "X" (to close the modal). When the "Finished Reading" button is clicked, the modal updates the end date and the read status of the corresponding book in the library array. The function then calls storeLibrary to update the "library" item in local storage, calls getLibrary to retrieve and display the updated library, and removes the modal. */
 function updateModal(event) {
   let updateModalBack = document.createElement("div");
   let updateModalCard = document.createElement("div");
@@ -149,14 +181,22 @@ function updateModal(event) {
   let modalInput = document.createElement("input");
   let updateBtn = document.createElement("button");
   let close = document.createElement("button");
+  let img = document.createElement("img");
   let tempID = event.target.parentElement.id;
+  console.log("TEMPID HERE: ", tempID);
 
+  img.src = event.target.parentElement.getAttribute("imgURL");
+  img.alt = event.target.parentElement.getAttribute("imgALT");  
+  img.height = event.target.parentElement.getAttribute("imgH");
+  img.width = event.target.parentElement.getAttribute("imgW");
+  
   modalInput.type = "date";
   modalInput.name = "modalInput";
   modalInput.id = "modalInput";
   modalInputLabel.for = "modalInput";
   modalInputLabel.textContent = "Date Finished";
   updateBtn.textContent = "Finished Reading";
+   
 
   close.textContent = "X";
   close.addEventListener("click", () => {
@@ -188,29 +228,39 @@ function updateModal(event) {
   modalForm.appendChild(modalInput);
   modalForm.appendChild(updateBtn);
   updateModalCard.appendChild(close);
+  updateModalCard.appendChild(img);
 }
 
-function fetchBookID() {
-  let tempData = [];
+async function fetchImage(url) {
+  const img = new Image();
+  return new Promise((res, rej) => {
+    img.onload = () => res(img);
+    img.onerror = (e) => rej(e);
+    img.src = url;
+  });
+}
 
-  fetch(`https://openlibrary.org/search.json?q=title:Redwall&limit=1`)
+async function fetchAndSetBookCover(title, img) {
+  let tempID = 0;
+  await fetch(`https://openlibrary.org/search.json?q=title:${title}&limit=1`)
     .then((res) => res.json())
-    .then((data) => {      
-      return tempData = data;
+    .then((data) => {
+      console.log(data);
+      console.log(data.docs[0].cover_i);
+      tempID = data.docs[0].cover_i;
     })
     .catch((err) => console.error(err));
-    
+
+  img = await fetchImage(`https://covers.openlibrary.org/b/id/${tempID}-M.jpg`);
+
+  console.log("img: ", img);
+  library.forEach((a) => {
+    if (title == a.title && img.src.includes("")) {
+      a.imgURL = img.src;
+      a.imgH = img.height;
+      a.imgW = img.width;
+    }
+  });
+  storeLibrary();
 }
 
-function fetchCoverArt(id) {
-
-let tempData = [];
-var tempImg = new Image();
-
-fetch(`https://covers.openlibrary.org/b/id/2325082-M.jpg`)
-.then((res) => tempImg.src = res)
-.then ()
-
-}
-
-fetchBookID();
